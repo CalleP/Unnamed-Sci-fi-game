7f0d53969d0e673bce95d8222c0b217a8e34577e
Calle Persson <callepersson94@telia.com>
2015-06-03 19:12:50 +0200

    Corridors and lightning configuration

---
+++ Code red/Assets/FadingLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadingLight : MonoBehaviour {
+
+	public KeyCode plusintensity;
+	public KeyCode negintensitivity;
+
+	public KeyCode bouncen;
+	public KeyCode bouncep;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update(){
+		if(Input.GetKey(plusintensity)){
+			gameObject.GetComponent<Light>().intensity += 0.01f;
+		}
+		if(Input.GetKey(negintensitivity)){
+			gameObject.GetComponent<Light>().intensity -= 0.01f;
+		}
+
+
+	}
+}
