cd099bfdd0ff20b649fa90d0b748d688f48b959a
Calle Persson <callepersson94@telia.com>
2015-06-17 20:52:01 +0200

    Super basic ventilation

---
+++ Code red/Scripts/Ventilation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ventilation : MonoBehaviour {
+
+    public bool Enabled = true;
+    public List<Room> ConnectedRooms;
+    public float TransferRate = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enabled)
+        {
+            foreach (var room in ConnectedRooms)
+            {
+                room.Oxygen += TransferRate / ConnectedRooms.Count;
+            }
+        }
+
+	}
+
+    
+}
